694fcb848e6bf9f8362c49d85702639288dacb76
Edgar Castillo <edgar101999.eacm@gmail.com>
2022-08-23 17:53:25 -0700

    Init

---
+++ accounts/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUser(AbstractUser):
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.CASCADE,
+        black=True,
+        null=True
+    )
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
